dae4ebb8dab615c7861a73a48ad2cafef2708702
--help <leandro.g.almeida@gmail.com>
2022-11-30 14:51:10 -0600

    add general rename command

---
+++ src/asunder/rope_sdk/__init__.py
@@ -0,0 +1,3 @@
+from asunder.rope_sdk.refactor.rename.rename import rename, rename_module
+
+__all__: list[str] = ["rename_module, rename"]
--- src/asunder/utils/logging.py
+++ src/asunder/utils/logging.py
@@ -1,20 +1,47 @@
-from logging import Logger, getLogger
-from typing import cast
+from logging import WARNING, Logger, getLogger
+from typing import Optional, cast
 
 from rich.console import Console
 from rich.logging import RichHandler
 
 
-def get_logger_console() -> tuple[Logger, Console]:
+def _set_up_logger(console: Optional[Console] = None) -> Logger:
+    """
+    Log to console with a simple formatter; used by CLI
+
+    Parameters
+    ----------
+    console : Console
+        rich console with style and colored
+
+    Returns
+    -------
+    logging.Logger
+        logger with the CLI name
+
+    """
+    if not console:
+        console = Console()
+    module_logger = getLogger("asunder")
+    module_logger.addHandler(RichHandler(rich_tracebacks=True, console=console))
+    module_logger.setLevel(level=WARNING)
+
+    return module_logger
+
+
+def get_logger_console(console: Optional[Console] = None) -> tuple[Logger, Console]:
+
+    if not console:
+        console = Console()
 
     logger = getLogger("asunder")
 
     if len(logger.handlers) > 0:
-
         handler: RichHandler = cast(RichHandler, logger.handlers[0])
         console = handler.console
         print("handler names = ", [handle.__dict__ for handle in logger.handlers])
     else:
-        raise AttributeError("No Handlers in logger")
+        logger = _set_up_logger(console)
+        logger.warning("no rich handlers in logger")
 
     return logger, console
